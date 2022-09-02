1c57bd6e436dde73858780d6e5678420f916c74c
Bodzio-2 <kbodzio@student.agh.edu.pl>
2022-09-02 17:10:34 +0200

    some test stuff

--- d2Request.py
+++ d2Request.py
@@ -6,30 +6,37 @@ BASE_URL = "https://www.bungie.net/Platform"
 headers = {
     "x-api-key": 'c0fb3fa2aa5b4e66a63f3c0af2443885'
 }
+#BODZIO
+#membershipType = "254"
+#membershipId = "21023690"
+#destinyMembershipId = "4611686018463887794"
+#destinyMembershipType = "2"
 
+#DUSZEK
 membershipType = "254"
-membershipId = "21023690"
-destinyMembershipId = "4611686018463887794"
-destinyMembershipType = "2"
+membershipId = "20587488"
+destinyMembershipId = "4611686018486261830"
+destinyMembershipType = "3"
+
 
 query_params_LinkedMembership = {
     "getAllMemberships": 'true'
 }
 
-#getLinkedProfiles = requests.get(f"{BASE_URL}/Destiny2/{membershipType}/Profile/{membershipId}/LinkedProfiles/", headers=headers, params=query_params)
-
+#getLinkedProfiles = requests.get(f"{BASE_URL}/Destiny2/{membershipType}/Profile/{membershipId}/LinkedProfiles/", headers=headers, params=query_params_LinkedMembership)
+#print (getLinkedProfiles.json())
 query_params_Components = {
-    "components": '200,205'
+    "components": '205'
 }
 
 
-#getProfile = requests.get(f"{BASE_URL}/Destiny2/{destinyMembershipType}/Profile/{destinyMembershipId}/", headers=headers, params=query_params_Components)
+getProfile = requests.get(f"{BASE_URL}/Destiny2/{destinyMembershipType}/Profile/{destinyMembershipId}/", headers=headers, params=query_params_Components)
 
-#with open('response.json', 'w') as json_file:
-#    json.dump(getProfile.json(), json_file)
+with open(f'{destinyMembershipId}.json', 'w') as json_file:
+    json.dump(getProfile.json(), json_file)
 
-getManifest = requests.get(f"{BASE_URL}/Destiny2/Manifest/", headers=headers)
+#getManifest = requests.get(f"{BASE_URL}/Destiny2/Manifest/", headers=headers)
 
-with open('manifest.json', 'w') as json_file:
-    json.dump(getManifest.json(), json_file)
+#with open('manifest.json', 'w') as json_file:
+#    json.dump(getManifest.json(), json_file)
 
--- sqliteTest.py
+++ sqliteTest.py
@@ -4,6 +4,11 @@ import json
 from PIL import Image
 import requests
 
+BASE_URL = "https://www.bungie.net/Platform"
+
+headers = {
+    "x-api-key": 'c0fb3fa2aa5b4e66a63f3c0af2443885'
+}
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -42,10 +47,44 @@ def displayDamageTypes(conn):
 
             im = Image.open(f'{NAME}.png')
             im.show()
-            
-        
+
+def listEquippedItems(destinyMembershipType, destinyMembershipId, characterId):
+    query_params_Components = {
+        "components": '205'
+    }
+
+    getProfile = requests.get(f"{BASE_URL}/Destiny2/{destinyMembershipType}/Profile/{destinyMembershipId}/", headers=headers, params=query_params_Components)
+
+    itemsHash = []
+
+    for item in getProfile.json()['Response']['characterEquipment']['data'][f'{characterId}']['items']:
+        itemsHash.append(item['itemHash'])
+
+    return itemsHash
+
+def displayEquippedWeaponNames(conn, weaponIDs):
+    TABLE = "DestinyCollectibleDefinition"
+    for id in weaponIDs:
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM {TABLE} WHERE json LIKE '%{id}%'")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            jsonRow = json.loads(row[1])
+            print(jsonRow['displayProperties']['name'])      
 
 database = r"world_sql_content_eab836f0f0bd68f7ac09055c2e38fe14.sqlite3"
 manifest = create_connection(database)
 
-displayDamageTypes(manifest)
+#displayDamageTypes(manifest)
+#displayEquippedWeaponNames(manifest)
+
+equippedListBod = listEquippedItems("2", "4611686018463887794", "2305843009366923962")
+#print(equippedList)
+print("Bodzio:")
+displayEquippedWeaponNames(manifest, equippedListBod)
+
+equippedListDusz = listEquippedItems("3", "4611686018486261830", "2305843009417904195")
+print("Duszek:")
+displayEquippedWeaponNames(manifest, equippedListDusz)
